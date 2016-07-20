e056c31c4dadc7d7e14c6985425236f510ebb8b1
Armenio Antunes <Armenio.Antunes@truphone.com>
2016-07-20 18:10:00 +0100

    spring lib finally included

--- TrainSpringboot/src/main/java/com/armenio/training/Server.java
+++ TrainSpringboot/src/main/java/com/armenio/training/Server.java
@@ -12,7 +12,7 @@ public class Server {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello World!";
+		return "<html><header><title>baam</title></header><body><h3>Hello Maria! Eu sou o servidor no rapberry pi da tua sala! Up and Running! yeeeeeeeyyyy! :)</h3><img style=\"-webkit-user-select: none\" src=\"http://vignette2.wikia.nocookie.net/justdance/images/1/1c/Post-18760-lion-king-meerkat-hula-dancing-5zsd.gif/revision/latest?cb=20150714232455\"></body></html>";
 	}
 
 	public static void main(String[] args) throws Exception {
